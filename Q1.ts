689abf6bc57f384ec33a386116938154308adc68
Rizwan Ansari <160248118+ali-rizwaan@users.noreply.github.com>
2024-02-16 06:24:09 -0800

    Add files via upload

---
+++ Q1.ts
@@ -0,0 +1,5 @@
+//1. Install Node.Js, Typescript And Vs Code On Your Computer.
+1.//https://code.visualstudio.com/download for(vs code)
+2.//nodejs.org/en/download for(nodejs)
+3.//npm i -g typescript for (typescript)
+4.//tsc -- init for (tsconfig.json)
